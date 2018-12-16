b08f98de056686f7d2368ea46a12dd8fb5ed9a1f
Raiondesu <a.raiondesu@gmail.com>
2018-12-17 02:12:03 +0300

    ⭐new(test): add test case for PR #481

--- test/unit/silent.test.js
+++ test/unit/silent.test.js
@@ -1,18 +1,49 @@
 describe('silent', () => {
-  it('should be suppressed translate warnings', () => {
+  it('should suppress translate warnings', () => {
     const vm = new Vue({
       i18n: new VueI18n({
         locale: 'en',
+
         silentTranslationWarn: true,
         messages: {
           en: { who: 'root' },
           ja: { who: 'ルート' }
+        },
+
+        dateTimeFormats: {
+          'en': {
+            short: {
+              year: '2-digit',
+              month: 'short',
+              day: 'numeric',
+              hour: 'numeric',
+              minute: 'numeric'
+            },
+            human: {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+              hour: 'numeric',
+              minute: 'numeric'
+            }
+          }
+        },
+        numberFormats: {
+          'en': {
+            test: {
+              style: 'currency',
+              currency: 'USD',
+              currencyDisplay: 'name'
+            }
+          }
         }
       })
     })
 
     const spy = sinon.spy(console, 'warn')
     vm.$t('foo.bar.buz')
+    vm.$d(1545001459635, 'short')
+    vm.$n(12, 'test')
     assert(spy.notCalled === true)
 
     // change
