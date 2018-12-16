fd8d19657c5cd7c1092ddcfd958237011efcbc79
Raiondesu <a.raiondesu@gmail.com>
2018-12-17 00:16:48 +0300

    🐛bugfix(karma): check for OS before adding Safari to tests
    Safari does not exist on platforms other than `darwin` (MacOS),
    so starting production tests on them always ends with error.

--- config/karma.unit.conf.js
+++ config/karma.unit.conf.js
@@ -1,14 +1,18 @@
 const base = require('./karma.base.conf')
 
+// Launch safari only on macs
+const hasSafari = process.platform === 'darwin'
+
 module.exports = config => {
   config.set(Object.assign(base, {
-    browsers: ['Chrome', 'Firefox', 'Safari'],
+    browsers: ['Chrome', 'Firefox'].concat(hasSafari ? ['Safari'] : []),
     reporters: ['progress'],
     singleRun: true,
     plugins: base.plugins.concat([
       'karma-chrome-launcher',
       'karma-firefox-launcher',
+    ]).concat(hasSafari ? [
       'karma-safari-launcher'
-    ])
+    ] : [])
   }))
 }
